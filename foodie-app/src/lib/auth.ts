200404d190370318f52d676dd1704deffe2c2734
kishanr <kishanr2318@gmail.com>
2026-09-12 17:13:19 +0530

    fix: bcrypt import for Vercel edge/node runtime compatibility

--- foodie-app/src/lib/auth.ts
+++ foodie-app/src/lib/auth.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 import { SignJWT, jwtVerify } from 'jose';
 import { cookies } from 'next/headers';
 import { prisma } from './db';
